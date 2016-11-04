7b086ed9ebdd8784e3b6a02b7bcbe86a92960a7a
Shweta <srk.sdm@gmail.com>
2016-11-04 09:55:02 -0700

    Assigment 4

---
+++ Homework4/Factorial_1_to_n/Factorial_1_to_n/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//program to find factorial of 1 to n
+
+namespace Factorial_1_to_n
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=0,i=0,fact=1,k=1;
+            //n- number of factoials to be displayed
+            //fact - to store factorial
+            //i and k for looping
+      
+            Console.Write("Enter a small positive integer : ");
+
+            string str = Console.ReadLine();
+
+            n = int.Parse(str); 
+             
+            if (n==0)
+             {
+               Console.WriteLine("Factorial of 0 : {0}", fact);
+             }
+
+           for (i = 1; i <= n; i++,k++)
+            {
+                fact *= i;
+
+                Console.WriteLine("Factorial of {0} = {1}", k , fact);
+              
+            }
+
+            Console.ReadLine();
+          }
+
+        
+    }
+}
